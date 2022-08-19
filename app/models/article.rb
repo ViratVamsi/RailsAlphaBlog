a8773720aa792bf9a2e2d9e8e73d28d1072f2514
Vamsi Prasad Kumili <vkumili@opentext.com>
2022-08-19 21:51:15 +0530

    Added bootstrap to Alpha-blog

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
+    validates :title, presence: true, uniqueness: true
     validates :description, presence: true, length: {minimum:6, maximum:255}
 end
