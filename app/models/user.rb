ce5a3ba262c830faa594b326c601fcffe465562c
Vamsi Prasad Kumili <vkumili@opentext.com>
2022-08-20 19:19:04 +0530

    before_save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+    before_save {self.email = email.downcase}
+
     validates :username, presence: true, 
                 length: {minimum: 3, maximum: 25}, 
                 uniqueness: true
