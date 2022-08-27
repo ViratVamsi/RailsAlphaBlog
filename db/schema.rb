c36fae29594ce34a48fd5c9b7195d1943e0e29ef
Vamsi Prasad Kumili <vkumili@opentext.com>
2022-08-27 12:12:13 +0530

    Defined many-many relationship and added view pages to alpha blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_132831) do
+ActiveRecord::Schema.define(version: 2022_08_27_045405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer "user_id"
   end
 
